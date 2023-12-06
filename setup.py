6925d89c5f5d814cd69ecd235809ebfd4deaa651
VismayaM-2003 <128399904+VismayaM-2003@users.noreply.github.com>
2023-12-06 11:56:44 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup
-setup(name='testproject',
-version='0.1',
-description='Testing installation of Package',
-url='https://github.com/VismayaM-2003/Package-to-PyPI.git',
-author='Vismaya',
-author_email='muruganvichunni@gmail.com',
-license='MIT',
-packages=['src'],
-zip_safe=False)
+setup(
+    name='functionsproject',
+    version='0.1',
+    description='Testing installation of Package',
+    url='',
+    author='Vismaya',
+    author_email='muruganvichunni@gmail.com',
+    license='MIT',
+    packages=['src'],
+    zip_safe=False
+)
